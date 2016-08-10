e5f1aa9f18c1594a0c81a68eb5df02d0964564af
Gregor Woiwode <gw@co-it.eu>
2016-08-10 12:27:11 +0200

    Adds test skeleton for priority queue

---
+++ katas/PriorityQueue/Queue.spec.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PriorityQueue
+{
+  /// <summary>
+  /// nFixt   - Creates a new TestFixture
+  /// nTest   - Creates a new Test
+  /// nAssert - Adds Assertion inside Test
+  /// </summary>
+  [TestFixture]
+  internal class QueueSpecs
+  {
+  }
+}
